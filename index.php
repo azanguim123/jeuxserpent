596d891cec373d2d3c5020164a84122e440b013f
nelson_azanguim <62788763+azanguim123@users.noreply.github.com>
2024-01-08 21:08:15 +0300

    prejetSerpent

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jeu du Serpent</title>
+	<script src="script.js"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1>by Larry@Nelson</h1>
+	<p><span class="info">!!! </span> Ce Jeu se jou avec les Touches de directions du Clavier</p>
+
+</body>
+</html>
